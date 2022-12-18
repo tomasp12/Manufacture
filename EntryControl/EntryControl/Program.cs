2a08db567c0499c9198570e4fd966a22967fdeb6
Tomas Pranka <tomas.pranka@codeacademylt.onmicrosoft.com>
2022-12-18 21:49:52 +0200

    Creating new class GenerateReportWorkHours, this class gets data from Event table calculate working time in seconds and save data to ReportWorkHours table.

--- EntryControl/EntryControl/Program.cs
+++ EntryControl/EntryControl/Program.cs
@@ -2,3 +2,7 @@
 // Filling up Event Database
 var simulate = new SimulateMovement();
 simulate.Simulate();
+
+// Make report off worked hours and saving all data to ReportWorkHours database, work time in seconds
+var reportWorkHours = new GenerateReportWorkHours();
+reportWorkHours.Generate();
