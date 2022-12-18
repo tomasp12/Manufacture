bd82c999af3d289de294591619413d0e3f6e5ab7
Tomas Pranka <tomas.pranka@codeacademylt.onmicrosoft.com>
2022-12-18 21:43:54 +0200

    Adding new Class SimulateMovement, this class create data to Event database

--- EntryControl/EntryControl/Program.cs
+++ EntryControl/EntryControl/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EntryControl;
+// Filling up Event Database
+var simulate = new SimulateMovement();
+simulate.Simulate();
